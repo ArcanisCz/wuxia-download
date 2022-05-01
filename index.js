d18f5af46ff77a1d69be694022ca39a161d8b032
ArcanisCz <arcaniscz@gmail.com>
2022-05-01 12:47:26 +0200

    last version before site update

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ const superagent = require("superagent");
 // getRecord(596, 795);
 // getRecord(796, 1124);
 // getRecord(1125, 1265);
-getRecord(1266, 1357);
+// getRecord(1266, 1357);
+// getRecord(2257, 2451);
+
+getNecro(1051, 1051);
 
 function getAwe(from, to) {
     const tasks = [];
@@ -90,6 +93,20 @@ function getRecord(from, to) {
     return runSerial(tasks).then((promises) => writeFile("record-" + from + "-" + to, promises.join("")));
 }
 
+function getNecro(from, to) {
+    const tasks = [];
+    const f = i => () => {
+        return getBody(`/novel/necropolis-immortal/necro-chapter-${i}`)
+          .then(parseBody)
+          .then(makeHtml)
+          .then(x => new Promise(resolve => setTimeout(() => resolve(x), randMilis())));
+    };
+    for (let i = from; i <= to; i++) {
+        tasks.push(f(i));
+    }
+    return runSerial(tasks).then((promises) => writeFile("necro-" + from + "-" + to, promises.join("")));
+}
+
 
 function runSerial(tasks) {
     const concat = list => Array.prototype.concat.bind(list);
@@ -102,6 +119,7 @@ function parseBody(html) {
     const $ = cheerio.load(html);
     const title = $(".caption img + h4");
     const a = $($("body").find('.panel-default .fr-view'));
+
     return Promise.resolve({
         title: title.hasClass("text-spoiler") ? "" : title.html(),
         spoilerTitle: title.hasClass("text-spoiler") ? title.html() : "",
@@ -128,9 +146,12 @@ function writeFile(name, content) {
 function getBody(path) {
     return new Promise(function (resolve, reject) {
         superagent.get(`https://www.wuxiaworld.com${path}`)
+            // .set("Cookie", "WuxiaWorld.Auth=CfDJ8CO0SncLTtpIpyTeLBgDP0xNKECqC8vxalyqPAsNoiiohPOenAi18bTfCkH0OVT1M3-tq3z1SmFcWO1UMxvsxF7OEC66y2k-EEOG2gMSqJItQciGVMD_HHZlv6asfBsyieHo8iM3ASs8tLaO_3vMqOpx_xbrQBCB7oUZ4ofSyP_jhyS1Xg-oHgKN6IrkppNM7_jZgbzUHKfLx77D6NBCTw3HAB68Ssa6uyrePQwsOn48cKOqYxm3aQrwo9zXNZeGSbZtZGd_uzIQaW2VMqcgKQSYdpd6dgzuP4TsR787aetBNela7pMCwxu2QDieLtoDiBSJ5cs-MCA2icau9fieYaWjvV0dO72yUj9NGMgwE_yNyOrZiUra9DNxZftLrFEMvGdLjXNgOsvfZ6EMKliNvjqy9hfeeOQOKS9nQoZ_vxv-TMUS1tvWaKVslRyope0VkNeWkMD4XJsCrmh4o8wXaoiWm9t06e-QXTlnXOLQQ-kGR8UNWfL33RKE_70Os19v-eGNYjWZ9fm5EvcqjsOmcpCxxW97ki-4vWi-nyRk9UAg71o_7jYlYoXV0Gt6I3b-Fek5mApbSQOfFxb8XM4Ckym7vEozgqaJaORSippnYijXfdl-WmV0qkuDy4x58olMkAe1ac2cgNWtVpT11hDQ7po0DpShGdFPA3G2sbS1TDSpAOR1gcyRGOcL6YRbvF__WfmiklgDehSD7eu8uxw91TATYEatM5aKZafuDJU9bS5It0CyrwAfmcxpnEl0RRTPXX_6QbwlmB-XSaPKoolD-mlw_qYgmTEwvjEm-zws4e_ZxxYHY-qpSPtdoqnjq3wtqcPxDotCL5v0ViTSDRjY6ss5zafDBb8KF1YXacYRyITcK-PCfwTzLexvFH9WLSqZp1LkpUPxtX-L7YaHJ_XvDzrqIaNS4Ifnx7qmEKfwvMXsSUTNJmOsjb1q6ARXhIswkI2OxNA4yUpAKVDoFhQXRrzcpM1BHI63MB54cSAu-rKihKceLZzDFzcjGXA698FX0apMkWcv-wFv3_lat3GjfWq8h8F5A0hvgKE4wAue2yL8")
+            .set("Cookie", "WuxiaWorld.CsrfToken=CfDJ8CO0SncLTtpIpyTeLBgDP0zs6gb-N3OqqLdYZYEWdJSXgFPnxl4a4oxV4NbMtFoxNhfj5whsobO7Z_4PScGSBsddd7-0MvcSvVncYfgAvLhEiyW2wvLNSw3XIRYibfis0qZU7OQBOcEkxa5rRHhUFEU")
+            .withCredentials()
             .then((res) => {
                 console.log("done", path);
-                // console.log(res.text);
+                console.log(res.text);
                 resolve(res.text)
             })
             .catch((err) => {
